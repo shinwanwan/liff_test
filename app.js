72d37a9485a49b950687fbfeb01daa05efedc0c9
shinwanwan <32068981+shinwanwan@users.noreply.github.com>
2025-07-18 10:32:31 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -8,13 +8,27 @@ async function main() {
         return;
     }
 
+
+
     const profile = await liff.getProfile();
     const accessToken = await liff.getAccessToken();
+
+
 
     document.getElementById("userId").innerText = `User ID: ${profile.userId}`;
     document.getElementById("userName").innerText = `User Name: ${profile.displayName}`;
     document.getElementById("userStatus").innerText = `User Status: ${profile.statusMessage}`;
     document.getElementById("accessToken").innerText = `Access Token: ${accessToken}`;
+
+    if (profile.userId == "Udc176173617583df213e82f5bcdf61c2")
+    {
+        document.getElementById("verify").innerText = `Access Token: True`;
+    }
+    else
+    {
+        document.getElementById("verify").innerText = `Access Token: False`;
+    }
+
 
     console.log(profile);
 }
